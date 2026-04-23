68db28f53d0e38338867e8b649d1be473d88812e
Isaac Guzman <guzmanisaac2005@outlook.com>
2026-04-23 15:45:14 -0500

    feat: cree el  menu hambuerguesa para movil

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,10 @@ const box = document.getElementById("box-solution");
 btn.addEventListener("click", () => {
     box.classList.toggle("active");
 });
+
+const hamburger = document.getElementById("hamburger")
+const links = document.getElementById("navbar")
+
+hamburger.addEventListener("click", () =>{
+    links.classList.toggle("show")
+})
