a92521467bd06455dcb3c0199026ecfa66983c12
Isaac Guzman <guzmanisaac2005@outlook.com>
2026-04-24 15:52:08 -0500

    refactor:Mejora del css

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,13 @@
-const btn = document.getElementById("mover-btn");
-const box = document.getElementById("box-solution");
-
-btn.addEventListener("click", () => {
-    box.classList.toggle("active");
-});
-
 const hamburger = document.getElementById("hamburger")
 const links = document.getElementById("navbar")
 
 hamburger.addEventListener("click", () =>{
     links.classList.toggle("show")
 })
+
+const btn = document.getElementById("mover-btn");
+const box = document.getElementById("box-solution");
+
+btn.addEventListener("click", () => {
+    box.classList.toggle("active");
+});
